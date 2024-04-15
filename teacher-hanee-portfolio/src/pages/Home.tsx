ec8c9ed052decc02e2d38a354969a99d8765027c
unknown <davedalimocon@gmail.com>
2024-04-15 13:01:44 +0800

    header

--- teacher-hanee-portfolio/src/pages/Home.tsx
+++ teacher-hanee-portfolio/src/pages/Home.tsx
@@ -1,12 +1,16 @@
-import Header from "../components/Header"
+import Header from "../components/Header/Header"
 import SubHeader from "../components/SubHeader"
+import "../index.css"
 
 const Home = () => {
   return (
+  
     <div style={{display:"flex", justifyContent:"center",alignItems:"center", flexDirection:"column",gap:"75px"}}>
             <Header/>
             <SubHeader/>
+            <SubHeader/>   <SubHeader/>   <SubHeader/>   <SubHeader/>   <SubHeader/>   <SubHeader/>   <SubHeader/>   <SubHeader/>   <SubHeader/>   <SubHeader/>   <SubHeader/>   <SubHeader/>
     </div>
+    
   )
 }
 
